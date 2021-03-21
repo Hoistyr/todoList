642f27d2f7f9755c55f347c9c7a088a5144f9888
Hoistyr <hoistyd@gmail.com>
2021-03-21 16:12:37 -0600

    Added example todo items, updated visuals

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -1,34 +1,104 @@
+import {format} from 'date-fns';
+
 const createIninitalPageStructure = () => {
     const content = document.querySelector('#content');
-    const taskList = document.createElement('div');
-    taskList.id = 'taskList';
-    const taskInformation = document.createElement('div');
-    taskInformation.id='taskInformation';
+    const todoList = document.createElement('div');
+    todoList.id = 'todoList';
+    const todoInformation = document.createElement('div');
+    todoInformation.id='todoInformation';
+    content.append(todoList, todoInformation);
 
-    content.append(taskList, taskInformation);
+    //Todo list
+    createTodoListStructure();
+    
+    //Todo information
+    createTodoInformationStructure();
 }
 
-const createTaskListStructure = () => {
-    console.log('intaskListStructure');
-    const taskList = document.querySelector('#taskList');
-    const taskListNav = document.createElement('div');
-    taskListNav.id = 'taskListNav';
+const createTodoListStructure = () => {
+    //Creates the divs
+    const todoList = document.querySelector('#todoList');
+    const todoListNav = document.createElement('div');
+    todoListNav.id = 'todoListNav';
     
-    taskList.appendChild(taskListNav);
+    const todoListTodosHolder = document.createElement('div');
+    todoListTodosHolder.id = 'todoListTodosHolder';
+    todoList.append(todoListNav, todoListTodosHolder);
+    
+    //Runs the functions that populate the divs
+    populateTodoListNav();
+    populateTodoListTodosHolder();
+    populateTodoListTodosHolder();
+    populateTodoListTodosHolder();
+}
 
+const populateTodoListNav = () => {
+    const todoListNav = document.getElementById('todoListNav');
+    
     const hamburgerNavIcon = document.createElement('img');
     hamburgerNavIcon.classList.add('hamburgerNavIcon');
     hamburgerNavIcon.src = '../src/images/icons/hamburgerNavIcon.svg'
+    todoListNav.appendChild(hamburgerNavIcon);
+    
+    const todoListProjectName = document.createElement('h1');
+    todoListProjectName.id = 'todoListProjectName';
+    todoListProjectName.textContent = `All ToDo's`;
+    todoListNav.appendChild(todoListProjectName);
 
-    taskListNav.appendChild(hamburgerNavIcon);
-    
-    
-    //const hamburgerNavIcon
+    const newTodoInput = document.createElement('input');
+    newTodoInput.id = 'newTodoInput';
+    newTodoInput.type = 'search';
+    newTodoInput.name = 'newTodoInput'
+    newTodoInput.defaultValue = 'Enter a new ToDo item'
+    todoListNav.appendChild(newTodoInput);
+
+    newTodoInput.addEventListener('click', removeDefaultInput);
+
 }
 
-const createTaskInformationStructure = () => {
-    const taskInformation = document.querySelector('#taskInformation');
+const removeDefaultInput = () => {
+    const newTodoInput = document.getElementById('newTodoInput');
+    let inputText = newTodoInput.value;
+    if (inputText === 'Enter a new ToDo item') {
+        console.log('clearing input');
+        newTodoInput.value = ''
+        newTodoInput.removeEventListener('click', removeDefaultInput);
+    }
+}
+
+const populateTodoListTodosHolder = () => {
+    //Eventually this will import from an array from todo logic and populate the task list based on that array
+    
+    const todoListTodosHolder = document.getElementById('todoListTodosHolder');
+    
+    const exampleTodo = document.createElement('div');
+    exampleTodo.classList.add('todo');
+    const todoText = document.createElement('p');
+    todoText.classList.add('todoText');
+    todoText.textContent = 'This is an example todo';
+    exampleTodo.appendChild(todoText);
+
+    const currentDate = format(new Date(), 'MM/dd/yyyy');
+    console.log(currentDate);
+    const dueDate = document.createElement('p');
+    dueDate.classList.add('todoText', 'todoDueDate');
+    dueDate.textContent = currentDate;
+    exampleTodo.appendChild(dueDate);
+
+    console.log('by remove x');
+    const removeX = document.createElement('img');
+    removeX.src = '../src/images/icons/removeX.svg';
+    removeX.classList.add('removeX');
+    exampleTodo.appendChild(removeX);
+
+    todoListTodosHolder.appendChild(exampleTodo)
+}
+
+
+
+const createTodoInformationStructure = () => {
+    const todoInformation = document.querySelector('#todoInformation');
     
 }
 
-export { createIninitalPageStructure, createTaskListStructure, createTaskInformationStructure }
+export { createIninitalPageStructure }
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -3,7 +3,6 @@ import * as domFunctions from './domFunctions.js';
 const onPageLoad = (() => {
     const buildPageLayout = () => {
         domFunctions.createIninitalPageStructure();
-        domFunctions.createTaskListStructure();
     }
     return {
         buildPageLayout,
